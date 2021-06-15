cde0882d4fef2a3afe1d77c13ca51bc7a39a44c0
Algolbarth <62349521+Algolbarth@users.noreply.github.com>
2021-06-15 23:00:49 +0200

    Add files via upload

---
+++ Projet/cmake-build-debug/CMakeFiles/Projet.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "Projet_autogen"
+  "CMakeFiles/Projet_autogen.dir/AutogenOldSettings.txt"
+  "CMakeFiles/Projet.dir/src/MainWindow.cpp.o"
+  "CMakeFiles/Projet.dir/src/MyScene.cpp.o"
+  "CMakeFiles/Projet.dir/src/main.cpp.o"
+  "CMakeFiles/Projet.dir/Projet_autogen/mocs_compilation.cpp.o"
+  "Projet.pdb"
+  "Projet"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Projet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Projet/cmake-build-debug/CMakeFiles/Projet_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "Projet_autogen"
+  "CMakeFiles/Projet_autogen.dir/AutogenOldSettings.txt"
+  "CMakeFiles/Projet_autogen"
+  "Projet_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/Projet_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
